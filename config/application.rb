831977b3a0a0072907696c73275fd43e7494eae2
SrNascimento40 <wallacediogo10@hotmail.com>
2025-03-25 18:54:12 -0300

    undo last fix bug

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,6 @@ module Mistusica
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
     config.autoload_lib(ignore: %w[assets tasks])
-    
-    config.action_controller.allow_browser_request_format = true
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
